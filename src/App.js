35a603fcd812aeb25370aff60a12bf530a20fcb0
sinentantiso <103891865+sinentantiso@users.noreply.github.com>
2022-11-30 22:22:27 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-import React from "react";
+import React, {useState} from "react";
 import "./style.css";
+import axios from "axios"
 
 export default function App() {
+
+  const url = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=a317f1ed3617e08a94e2d851ebdb1b83"
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
