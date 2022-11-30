fec1090c7f69e0e644f10fed136ae3df40a77337
sinentantiso <103891865+sinentantiso@users.noreply.github.com>
2022-11-30 22:51:20 +0200

    1 file modified

--- src/App.js
+++ src/App.js
@@ -6,9 +6,19 @@ export default function App() {
 
   const url = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=a317f1ed3617e08a94e2d851ebdb1b83"
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+    <div className="App">
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>Durban</p>
+            <div className="temp">
+              <h1>50F</h1>
+            </div>
+            <div></div>
+          </div>
+        </div>
+      </div>
+      <div className="bottom"></div>
     </div>
   );
 }
